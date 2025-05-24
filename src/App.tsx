cf387cad9ea80c17fb6599322d35d3d8b00916c7
achano <hyuga.workplace@gmail.com>
2025-05-25 00:50:10 +0900

    提供資材反映

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import '@/app.css';
+import Layout from './Layout';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { Home } from './pages/Home';
+import NoteDetail from './pages/NoteDetail';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 export const App = () => {
-  const i = 0;
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <div className="h-screen">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/notes/:id" element={<NoteDetail />} />
+          </Route>
+          <Route path='signin' element={<Signin />} />
+          <Route path='signup' element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 };
